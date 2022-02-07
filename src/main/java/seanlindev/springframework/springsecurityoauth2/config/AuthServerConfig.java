e2e337a5f6b412cb347c4686f12384fd27ddbd3b
Sean Lin <seanlin>
2022-02-07 19:01:09 +0800

    enable refresh token grant type with authorization code

--- src/main/java/seanlindev/springframework/springsecurityoauth2/config/AuthServerConfig.java
+++ src/main/java/seanlindev/springframework/springsecurityoauth2/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package seanlindev.springframework.springsecurityoauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -15,6 +16,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
@@ -26,13 +30,14 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .withClient("client2")
                 .secret("222222")
                 .scopes("read")
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
                 .redirectUris("http://localhost:9090/auth")
                 ;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                 .userDetailsService(userDetailsService);
     }
 }
